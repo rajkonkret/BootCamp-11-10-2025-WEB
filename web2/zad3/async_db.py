13d37c640aae1f5ec1bbf84de752b9efa9290485
radoslawjaniak <rajkokret660@gmail.com>
2026-01-17 12:52:58 +0100

    [15 24] update web2 zad3

---
+++ web2/zad3/async_db.py
@@ -0,0 +1,19 @@
+import aiosqlite
+
+DB_FILE = "async_example.db"
+
+
+async def init_db():
+    async with aiosqlite.connect(DB_FILE) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL);
+        """)
+        await db.commit()
+
+
+async def add_task(title: str):
+    async with aiosqlite.connect(DB_FILE) as db:
+        await db.execute("INSERT INTO tasks (title) VALUES (?);", (title,))
+        await db.commit()
