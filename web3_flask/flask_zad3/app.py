9da5145451705dbc75f74c0704fb21f979c77d0f
radoslawjaniak <rajkokret660@gmail.com>
2026-01-17 14:29:41 +0100

    [15 37] update web3_flask/flask_zad3 add /offer

--- web3_flask/flask_zad3/app.py
+++ web3_flask/flask_zad3/app.py
@@ -3,10 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# http://127.0.0.1:5000/
 @app.route("/")
 def index():
     return f"<h1>Car Ads Portal</h1>"
 
+
+# http://127.0.0.1:5000/offer/audi/1000
+@app.route('/offer/<string:brand>/<int:price>')
+def offer(brand, price):
+    return f"<h1>You selected: {brand} price: {price}"
 
 
 if __name__ == '__main__':
