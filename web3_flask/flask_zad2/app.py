71a05209c6139a7d7c31f5e3cb4f9cf2b1733921
radoslawjaniak <rajkokret660@gmail.com>
2026-01-17 14:19:31 +0100

    [15 34] update web3_flask

---
+++ web3_flask/flask_zad2/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+# http://127.0.0.1:5000
+# http://127.0.0.1:5000/?color=blue
+# http://127.0.0.1:5000/?color=blue&style=italic
+@app.route("/")
+def index():
+    # b'color=blue'
+    print(request.query_string)
+    # print(request.args['color'])
+
+    color = "black"
+    if 'color' in request.args:
+        color = request.args['color']
+
+    style = "normal"
+    if 'style' in request.args:
+        style = request.args['style']
+
+    for p in request.args:
+        print(p, request.args[p])
+    # b'color=blue&style=italic'
+    # color blue
+    # style italic
+
+    return f'<h1 style="color: {color}; font-style:{style};">Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
