cabd8223ad64b6b65a413236b6225f63a9422e0b
radoslawjaniak <rajkokret660@gmail.com>
2026-01-17 14:41:32 +0100

    [15 38] update web3_flask/flask_zad3 add /offer

--- web3_flask/flask_zad3/app.py
+++ web3_flask/flask_zad3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +6,17 @@ app = Flask(__name__)
 # http://127.0.0.1:5000/
 @app.route("/")
 def index():
-    return f"<h1>Car Ads Portal</h1>"
+    # menu = f"""
+    #     Add new <a href="">offer</a><br>
+    #     Add Audi 10000 go <a href="offer/Audi/10000">here</a><br>
+    #     <img src="1.svg" alt="1.svg" alt="Sample"><br>
+    #     """
+    menu = f"""
+    Add new <a href="">offer</a><br>
+    Add Audi 10000 go <a href="{url_for('offer', brand="Audi", price=10000)}">here</a><br>
+    <img src="{url_for('static', filename='static/1.svg')}" alt="1.svg" alt="Sample"><br>
+    """
+    return f"<h1>Car Ads Portal</h1><br>{menu}"
 
 
 # http://127.0.0.1:5000/offer/audi/1000
