3915c6865ae770e6bbe658cd3a9dfd4adf2fe271
radoslawjaniak <rajkokret660@gmail.com>
2026-02-14 14:38:26 +0100

    [19 35] add django myproject

--- web4_django/myproject/myapp/urls.py
+++ web4_django/myproject/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home, list_posts
+from .views import home, list_posts, get_post
 
 urlpatterns = [
     path('', home, name="home"),  # endpoint '/'
-    path('posts/', list_posts, name="posts")
+    path('posts/', list_posts, name="posts"),
+    path('posts/<int:post_id>/', get_post),
 ]
