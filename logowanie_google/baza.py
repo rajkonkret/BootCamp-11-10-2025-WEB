383117faa3424f827ed33d77debabd0f2e2ce00e
radoslawjaniak <rajkokret660@gmail.com>
2026-02-14 09:15:28 +0100

    [19 2] add login_google

---
+++ logowanie_google/baza.py
@@ -0,0 +1,10 @@
+def init_db():
+    pass
+
+
+def get_user():
+    pass
+
+
+def add_user():
+    pass
---
+++ logowanie_google/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request, Depends, HTTPException, Cookie
+from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
+
+import os
+from dotenv import load_dotenv
+import httpx
+
+from baza import init_db, get_user, add_user
+
+from jose import jwt, JWTError
