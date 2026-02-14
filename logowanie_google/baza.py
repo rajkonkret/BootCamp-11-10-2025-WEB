4918975e4f56d00319417caacb251f56325aabc7
radoslawjaniak <rajkokret660@gmail.com>
2026-02-14 09:23:33 +0100

    [19 3] add login_google

--- logowanie_google/baza.py
+++ logowanie_google/baza.py
@@ -1,10 +1,37 @@
+import sqlite3
+
+
 def init_db():
-    pass
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE
+    """)
+
+    conn.commit()
+    conn.close()
 
 
-def get_user():
-    pass
+def get_user(email):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    c.execute("""
+    SELECT id, email FROM users WHERE email=?
+    """, (email,))
+    user = c.fetchone()
+
+    # conn.commit()
+    conn.close()
+    return user
 
 
 def add_user():
-    pass
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    conn.commit()
+    conn.close()
