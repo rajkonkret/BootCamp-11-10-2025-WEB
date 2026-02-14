8b7132353d74a9830b269cc161809078f81c4ce1
radoslawjaniak <rajkokret660@gmail.com>
2026-02-14 09:44:38 +0100

    [19 7] update login_google

--- logowanie_google/main.py
+++ logowanie_google/main.py
@@ -12,4 +12,18 @@ from jose import jwt, JWTError
 
 init_db()
 
+load_dotenv()
 app = FastAPI()
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_ALGO = "HS256"
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+BACK_URI = os.getenv("BACK_URI")
+
+print(BACK_URI)
+
+GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
+GOOGLE_USERINFO_URL = "https://openidconnect.googleapis.com/v1/userinfo"
