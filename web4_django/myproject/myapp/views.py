66635915ffb3f73a4d4a4a3792a4123538583247
radoslawjaniak <rajkokret660@gmail.com>
2026-02-14 14:16:46 +0100

    [19 30] add django myproject

--- web4_django/myproject/myapp/views.py
+++ web4_django/myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Post
 
 
 # Create your views here.
@@ -8,3 +9,19 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "myapp/index.html", {"message": "Django działa!"})
+
+
+def list_posts(request):
+    posts = Post.objects.all().order_by("-created_at")
+
+    data = [
+        {
+            "id": post.id,
+            "title": post.title,
+            "content": post.content,
+            "created_at": post.created_at,
+        }
+        for post in posts
+    ]
+
+    return JsonResponse(data, safe=False)
