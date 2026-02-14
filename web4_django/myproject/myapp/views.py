1d432ee242aef61b775e7ca14d96268f44fde907
radoslawjaniak <rajkokret660@gmail.com>
2026-02-14 13:59:57 +0100

    [19 29] add django myproject

--- web4_django/myproject/myapp/views.py
+++ web4_django/myproject/myapp/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse("Witaj w mojej aplikacji Django")
+
 def home(request):
-    return HttpResponse("Witaj w mojej aplikacji Django")
+    return render(request, "myapp/index.html", {"message": "Django działa!"})
